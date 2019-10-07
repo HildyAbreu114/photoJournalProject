d51bef0419e7497ce4f329c6b98d2470d1d1a7fe
HildyAbreu114 <52180598+HildyAbreu114@users.noreply.github.com>
2019-10-07 14:45:49 -0400

    Add files via upload

---
+++ photoJournal/photoJournal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  photoJournal
+//
+//  Created by hildy abreu on 10/7/19.
+//  Copyright © 2019 hildy abreu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
